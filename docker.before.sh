63077dc856480c1de33fa08b81f4556927418352
terekhin.konst@gmail.com <kterekhin@solvd.com>
2021-07-21 11:20:06 +0300

    New test-4

--- docker.before.sh
+++ docker.before.sh
@@ -1,2 +1,2 @@
-sudo rm -rf /home/ubuntu/app
-sudo docker-compose down
+cd /home/ubuntu/app && sudo docker-compose down
+cd /home/ubuntu/app && sudo docker-compose -f docker-compose.yml up -d --build
