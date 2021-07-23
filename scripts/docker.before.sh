1de6a65a2816cdf4e4762859b3d84d66b9e7239c
terekhin.konst@gmail.com <kterekhin@solvd.com>
2021-07-23 10:40:08 +0300

    script2

--- scripts/docker.before.sh
+++
@@ -1,2 +0,0 @@
-sudo cd .. && docker-compose down
-sudo docker-compose -f docker-compose.yml up -d --build
---
+++ scripts/docker_before.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>/home/ubuntu/out.log 2>&1
+
+sudo cd .. && docker-compose down
+sudo docker-compose -f docker-compose.yml up -d --build
